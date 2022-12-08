6c0b4e8657daa4afa3b25d867307c8e434a66da2
KJH <green931@hanmail.net>
2022-12-08 22:53:59 +0900

    3th

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<style scoped>  
+.fixed-bar {  
+  position: sticky;  
+  position: -webkit-sticky; /* for Safari */  
+  top: 0em;  
+  z-index: 2;  
+}  
+</style>  
+<template>  
+  <v-app>  
+    <!-- 툴바에 제목, 버튼을 삽입 -->  
+    <v-app-bar class="fixed-bar" color="green" dark>  
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>  
+      <v-toolbar-title>통신점검지침서</v-toolbar-title>  
+      <v-spacer></v-spacer>  
+      <v-btn icon>  
+        <v-icon>more_vert</v-icon>  
+      </v-btn>  
+    </v-app-bar>  
+    <v-content>  
+      <!-- 카드 그리드 사이에 여백 설정-->  
+      <v-container>  
+        <v-row>  
+          <!-- 기기별로 그리드의 크기 설정-->  
+          <v-col cols="12">  
+            <v-card style="width: 100%" height="100">  
+              <v-card-title>2020년 12월 4일 (일) 주간 A / 야간 B</v-card-title>  
+              2020년 12월 4일 (일) 주간 A / 야간 B  
+            </v-card>  
+          </v-col>  
+          <div> 
+            <v-slide-x-transition mode="out-in">  
+              <router-view></router-view>  
+            </v-slide-x-transition>  
+          </div> 
+          <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-for="item in this.aPictures" :key=item.key>  
+            <!-- 카드 UI에 사진 담아내기-->  
+            <v-card height="330px">  
+              <v-img :src="item.url" height="200">  
+              </v-img>  
+              <v-card-title>  
+                <h1 class="title grey--text text--darken-3   
+                  mb-3">{{item.title}}</h1>  
+                <p class="body-2 grey--text">{{item.info}}</p>  
+              </v-card-title>  
+            </v-card>  
+          </v-col>  
+        </v-row>  
+    </v-container> 
+  </v-content>  
+  </v-app>  
+</template>  
+<script>  
+  // 사진 정보(JSON 파일) 읽어오기  
+  import oPictureData from '@/datasources/picture-data'  
+  export default {  
+    data() {  
+      return {  
+        // aPictures의 배열에 사진 정보 저장하기  
+        'aPictures': oPictureData.aPictures  
+      }  
+    },  
+    name: 'App',  
+  }  
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue' 
+import Router from 'vue-router' 
+// main_page와 sub_page라는 컴포넌트 모듈 2개를 가져옴 
+import main_page from '@/components/main_page' 
+import sub_page from '@/components/sub_page' 
+Vue.use(Router) 
+export default new Router({ 
+  mode: 'history', 
+  base: process.env.BASE_URL, 
+  routes: [ 
+    { 
+      // 루트 페이지는 main_page로 설정해 둠. 
+      path: '/', name: 'home', component: main_page 
+    }, 
+    { 
+      path: '/main', name: 'main_page', component: main_page 
+    }, 
+    { 
+      path: '/sub', name: 'sub_page', component: sub_page 
+    } 
+  ] 
+})
